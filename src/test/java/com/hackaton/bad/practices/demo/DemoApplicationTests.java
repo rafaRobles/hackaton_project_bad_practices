255ea618681c32777413b881e8079e3097fdadb6
Carlos Robles <carlosrobles@MacBook-Pro-de-Carlos.local>
2026-07-20 19:34:06 -0600

    clases base

---
+++ src/test/java/com/hackaton/bad/practices/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.bad.practices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
